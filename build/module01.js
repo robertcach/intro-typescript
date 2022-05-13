367ccb5626296a9436a19e482fd68de3a8e27ebf
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-13 11:57:27 +0200

    type inference: number and string

--- build/module01.js
+++ build/module01.js
@@ -3,3 +3,12 @@ function addNumbers(x, y) {
     return x + y;
 }
 console.log(addNumbers(3, 6));
+/* TYPE INFERENCE */
+let x; // Explicitly declares x as a "number" type
+let x2 = 1; // Explicitly declares x as a "number" type and "1" value
+let y = 1; // Implicitly declares y as a "number" type
+let z; // Declares z without initializing it. It is "any" type
+// Explicitly declares bothTypes can be "number" or "string" type
+let bothTypes;
+bothTypes = 5;
+bothTypes = "five";
--- module01.ts
+++ module01.ts
@@ -3,3 +3,18 @@ function addNumbers(x: number, y: number) {
 }
 
 console.log(addNumbers(3, 6));
+
+
+
+/* TYPE INFERENCE */
+let x: number;   // Explicitly declares x as a "number" type
+let x2: number = 1 // Explicitly declares x as a "number" type and "1" value
+
+let y = 1;       // Implicitly declares y as a "number" type
+let z;           // Declares z without initializing it. It is "any" type
+
+
+// Explicitly declares bothTypes can be "number" or "string" type
+let bothTypes : number | string 
+bothTypes = 5
+bothTypes = "five"
