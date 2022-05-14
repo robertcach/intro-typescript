fa290d6a4962e510ff2dc0cfcd0a1fd5b70eebc2
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-14 14:15:37 +0200

    feat: any and unknown types

--- build/module01.js
+++ build/module01.js
@@ -17,8 +17,38 @@ var ContractStatus;
 (function (ContractStatus) {
     ContractStatus[ContractStatus["Permanent"] = 0] = "Permanent";
     ContractStatus[ContractStatus["Temp"] = 1] = "Temp";
-    ContractStatus[ContractStatus["Apprentice"] = 2] = "Apprentice";
+    ContractStatus[ContractStatus["Apprentice"] = 2] = "Apprentice"; // 2
 })(ContractStatus || (ContractStatus = {}));
 let employeeStatus = ContractStatus.Temp;
-console.log(employeeStatus);
+console.log(employeeStatus); // Show the value: 1
 console.log(ContractStatus[employeeStatus]); // Show the name associated with the enumeration: 'Temp'
+/* TYPE INFERENCE: ANY AND UNKNOW */
+// 'Any' doesn't make any test in compile time but we can get troubles in runtime
+let randomValue = 10;
+randomValue = 'Mateo'; // OK
+randomValue = true; // OK
+// unknown
+let randomValue2 = 10;
+randomValue2 = true; // Can change data type
+randomValue2 = 'Mateo';
+randomValue2 = { name: 'pepe' };
+//console.log(randomValue2.name);   // Error: Object is of type unknown
+//randomValue2();                  // Error: Object is of type unknown
+// randomValue2.toUpperCase();      // Error: Object is of type unknown
+// The differences is that we can't interact/access with variables of type "unknown"
+// In this way, is like we are protected of errors in runtime
+/* TYPE ASSERTION */
+// We can convert a 'unknown' type in other before to do any test or instruction
+// randomValue2.toUpperCase(); // Error
+// (randomValue2 as string).toUpperCase(); // OK
+// Other way to test if 'randomValue2' is a string type before call toUpperCase method
+let randomValueMy = 10;
+randomValueMy = true;
+randomValueMy = 'Robert';
+typeof randomValueMy === 'string' ? console.log(randomValueMy.toLowerCase()) : console.log('Error');
+/* if (typeof randomValueMy === "string") {
+    console.log((randomValueMy as string).toUpperCase());    //* Returns MATEO to the console.
+} else {
+    console.log("Error - A string was expected here.");    //* Returns an error message.
+}
+ */ 
--- module01.ts
+++ module01.ts
@@ -5,7 +5,6 @@ function addNumbers(x: number, y: number) {
 console.log(addNumbers(3, 6));
 
 
-
 /* TYPE INFERENCE: NUMBER AND STRING */
 let x: number;   // Explicitly declares x as a "number" type
 let x2: number = 1 // Explicitly declares x as a "number" type and "1" value
@@ -31,3 +30,41 @@ enum ContractStatus {
 let employeeStatus: ContractStatus = ContractStatus.Temp;
 console.log(employeeStatus); // Show the value: 1
 console.log(ContractStatus[employeeStatus]); // Show the name associated with the enumeration: 'Temp'
+
+
+
+
+/* TYPE INFERENCE: ANY AND UNKNOW */
+// 'Any' doesn't make any test in compile time but we can get troubles in runtime
+let randomValue: any = 10;
+randomValue = 'Mateo';   // OK
+randomValue = true;      // OK
+
+// unknown
+let randomValue2: unknown = 10;
+randomValue2 = true; // Can change data type
+randomValue2 = 'Mateo';
+randomValue2 = { name: 'pepe' };
+
+//console.log(randomValue2.name);   // Error: Object is of type unknown
+//randomValue2();                  // Error: Object is of type unknown
+// randomValue2.toUpperCase();      // Error: Object is of type unknown
+
+// The differences is that we can't interact/access with variables of type "unknown"
+// In this way, is like we are protected of errors in runtime
+
+
+
+
+/* TYPE ASSERTION */
+// We can convert a 'unknown' type in other before to do any test or instruction
+
+// randomValue2.toUpperCase(); // Error
+// (randomValue2 as string).toUpperCase(); // OK
+
+// Other way to test if 'randomValue2' is a string type before call toUpperCase method
+let randomValueMy: unknown = 10;
+randomValueMy = true;
+randomValueMy = 'Robert';
+
+typeof randomValueMy === 'string' ? console.log(randomValueMy.toLowerCase()) : console.log('Error');
