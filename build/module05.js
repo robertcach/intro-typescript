e4ecc70413bc6fbb9ae6429cf2bfed90a1028aff
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-16 13:03:29 +0200

    feat: extends classes

--- build/module05.js
+++ build/module05.js
@@ -46,9 +46,11 @@ class Car {
     turn(direction) {
         return `${this.worker()} is turning to ${direction}`;
     }
+    // protected to the subclasses can execute this method
     worker() {
         return this._make;
     }
+    // static method
     static getNumberOfCars() {
         return Car.numberOfCars;
     }
@@ -62,10 +64,33 @@ console.log(myCar1.color); // Access value THROUGH get and set accessors and def
 let myCar2 = new Car('Galaxy Motors', 'red', 8);
 console.log(myCar2.doors);
 console.log(myCar2.doors = 5); // Error appears
-let myCar3 = new Car('Galaxy Motors', 'Gray');
+let myCar3 = new Car('Space Digital', 'Gray');
 console.log(myCar3.doors); // Shows 4. The default value
-console.log(myCar1.accelerate(35));
-console.log(myCar2.brake());
-console.log(myCar3.turn('right'));
-/* EXERCISE */
-console.log(Car.getNumberOfCars());
+console.log(myCar1.accelerate(35)); // Cool Car Company is accelerating to 35 MPH.
+console.log(myCar2.brake()); // Galaxy Motors is braking with the standard braking system.
+console.log(myCar3.turn('right')); // Space Digital is turning to right
+/* EXERCISE STATIC PROPERTIES */
+console.log(Car.getNumberOfCars()); // Shows 3 because we created 3 instances of Car class. All of that instances shares properties and methods statics
+/* EXTEND CLASSES */
+class ElectricCar extends Car {
+    // Constructor: the parameters it mas have all properties=> father class "Car" and this subclass "ElectricCar"
+    constructor(make, color, range, doors = 2) {
+        super(make, color, doors); // Include father class properties
+        this._range = range;
+    }
+    // Accessors
+    get range() {
+        return this._range;
+    }
+    set range(range) {
+        this._range = range;
+    }
+    // Methods
+    charge() {
+        console.log(this.worker() + " is charging");
+    }
+}
+let spark = new ElectricCar('Spark Motor', 'silver', 124, 2);
+console.log(spark.charge()); // Spark Motor is charging
+let eCar = new ElectricCar('Electric Car Co.', 'black', 263);
+console.log(eCar.doors);
