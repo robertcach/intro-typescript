0d2568818d373484a48db877b30a36a9e10e2afb
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-16 11:20:24 +0200

    feat: creating class part 2

--- module05.ts
+++ module05.ts
@@ -1,60 +1,64 @@
 /* CLASSES */
 class Car {
-    // Properties
-    _make: string;
-    _color: string;
-    _doors: number;
+  // Properties
+  _make: string;
+  _color: string;
+  _doors: number;
 
-    // Constructor
-    constructor(make: string, color: string, doors: number = 4) {
-      this._make = make;
-      this._color = color;
-      this._doors = doors;
+  // Constructor
+  constructor(make: string, color: string, doors: number = 4) {
+    this._make = make;
+    this._color = color;
+    this._doors = doors;
+  }
+
+  // Accessors
+  // set >> get
+
+  // get: read value (return it). Before we have to declare the value with 'set'. Don't have parameters
+  get make(): string {
+    return this._make;
+  }
+
+  // set: define a value to property. Must have parameters
+  set make(make: string) {
+    this._make = make;
+  }
+
+  get color(): string {
+    return 'The color of the car is ' + this._color;
+  }
+
+  set color(color: string) {
+    this._color = color
+  }
+
+  get doors(): number {
+    return this._doors;
+  }
+
+  set doors(doors: number) {
+    if (doors % 2 === 0) {
+      this._doors = doors
     }
-
-    // Accessors
-    get make() {
-      return this._make;
-    }
-
-    set make(make) {
-      this._make = make;
-    }
-
-    get color() {
-      return 'The color of the car is ' + this._color;
-    }
-
-    set color(color) {
-      this._color = color
-    }
-
-    get doors() {
-      return this._doors;
-    }
-
-    set doors(doors) {
-      if (doors % 2 === 0) {
-        this._doors = doors
-      }
-      throw new Error('Doors must be an even number')
-    }
+    throw new Error('Doors must be an even number')
+  }
 
 
-    // Methods
-    accelerate(speed: number): string {
-      return `${this.worker()} is accelerating to ${speed} MPH.`
-    }
+  // Methods
+  accelerate(speed: number): string {
+    return `${this.worker()} is accelerating to ${speed} MPH.`
+  }
 
-    brake(): string {
-        return `${this.worker()} is braking with the standard braking system.`
-    }
+  brake(): string {
+      return `${this.worker()} is braking with the standard braking system.`
+  }
 
-    turn(direction: 'left' | 'right') {
-      return `${this.worker()} is turning to ${direction}`
-    }
+  turn(direction: 'left' | 'right') {
+    return `${this.worker()} is turning to ${direction}`
+  }
 
-    worker(): string {
-      return this._make
-    }
+  worker(): string {
+    return this._make
+  }
 }
